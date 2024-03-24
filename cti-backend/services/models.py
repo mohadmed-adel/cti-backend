2bc5017d41521aacd72fe86590d2a801378cc2f7
Mohamed Adel <mohadel1033@gmail.com>
2024-03-24 21:28:10 +0200

    remove user

--- cti-backend/services/models.py
+++ cti-backend/services/models.py
@@ -1,6 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-
+ 
 # Create your models here.
 class MainService (models.Model):
     name = models.CharField(max_length=20)
@@ -15,7 +14,4 @@ class Service (models.Model):
     main_service = models.ForeignKey(MainService, models.DO_NOTHING,  blank=True, null=True,)
     def __str__(self):
         return self.name
-
-class User(AbstractUser):
-    # Your custom fields and methods here
-    pass
+ 
