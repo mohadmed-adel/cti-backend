22373b4a3d2c9d7a16cb3df0f63c8f4ef782c5e9
Mohamed Adel <mohadel1033@gmail.com>
2024-03-24 21:24:07 +0200

    pass

--- cti-backend/services/models.py
+++ cti-backend/services/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 # Create your models here.
 class MainService (models.Model):
     name = models.CharField(max_length=20)
@@ -14,3 +16,6 @@ class Service (models.Model):
     def __str__(self):
         return self.name
 
+class User(AbstractUser):
+    # Your custom fields and methods here
+    pass
