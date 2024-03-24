ced6d9bfab97ca6fa8c092fdeb2566c08670a81e
Mohamed Adel <mohadel1033@gmail.com>
2024-03-24 21:41:51 +0200

    fix

--- cti-backend/services/models.py
+++ cti-backend/services/models.py
@@ -1,5 +1,6 @@
 from django.db import models
- 
+from django.contrib.auth.models import AbstractUser
+
 # Create your models here.
 class MainService (models.Model):
     name = models.CharField(max_length=20)
@@ -15,3 +16,28 @@ class Service (models.Model):
     def __str__(self):
         return self.name
  
+
+class User(AbstractUser):
+    # Your custom fields and methods here
+
+    class Meta:
+        db_table = 'custom_user'  # Use a unique db_table name for your custom User model
+
+    # Override the groups field to specify a unique related_name attribute
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='custom_user_groups',
+        blank=True,
+        
+        
+    )
+
+    # Override the user_permissions field to specify a unique related_name attribute
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='custom_user_permissions',
+        blank=True,
+       
+    )
+
+    
