ed4e29f619bdd3af6e2b866f1c5d2042dcd478d7
Mohamed Adel <mohadel1033@gmail.com>
2024-03-24 20:54:30 +0200

    add req and doccer

--- services/models.py
+++ services/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class MainService (models.Model):
@@ -14,3 +15,13 @@ class Service (models.Model):
     main_service = models.ForeignKey(MainService, models.DO_NOTHING,  blank=True, null=True,)
     def __str__(self):
         return self.name
+
+
+
+
+class User(AbstractUser):
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=10)
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username", "first_name", "last_name"]
